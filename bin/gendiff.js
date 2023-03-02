2f98ec9e022a3731fbe36c8e5c246c1bae5f005f
A-Kimpo <112566801+A-Kimpo@users.noreply.github.com>
2023-03-02 17:39:05 +0500

    Create gendiff.js
    Import commander. Added help info on utility (description, version and arguments).

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .description('Compares two configuration files and shows a difference')
+  .version('1.0.0')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
+
+program.parse();
